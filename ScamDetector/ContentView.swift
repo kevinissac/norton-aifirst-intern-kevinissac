e3b751352b1e3bf2c789080f33eba0c70e45217d
Kevin Issac <kevinabyissac@gmail.com>
2026-04-17 19:01:38 +0100

    Initial Commit

---
+++ ScamDetector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScamDetector
+//
+//  Created by Kevin Issac on 17/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
---
+++ ScamDetector/ScamDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScamDetectorApp.swift
+//  ScamDetector
+//
+//  Created by Kevin Issac on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ScamDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
